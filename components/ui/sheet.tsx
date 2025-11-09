3de6e96643c4f817d6d25ddd486fcba9e4700b89
Kejsan Coku <kejsancoku@gmail.com>
2025-11-09 22:44:44 +0100

    fix: remove unused radix icon dependency

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as SheetPrimitive from "@radix-ui/react-dialog"
-import { Cross2Icon } from "@radix-ui/react-icons"
+import { X } from "lucide-react"
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
@@ -57,7 +57,7 @@ const SheetContent = React.forwardRef<
     >
       {children}
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none">
-        <Cross2Icon className="h-4 w-4" />
+        <X className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
     </SheetPrimitive.Content>
